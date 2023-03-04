26565be7f527a493ec9c8517f8feefbecf83ca87
nastaran-shafie <nastaran.sh33@gmail.com>
2023-03-04 12:44:07 +0330

    feat:add function to go frist panel page

--- Desktop/NasiShop/src/components/loginForm/input.tsx
+++ Desktop/NasiShop/src/components/loginForm/input.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../button";
 import Input from "../input";
 
 function LoginForm() {
+  const navigate = useNavigate();
+  function submit1() {
+    navigate("/panel/Management");
+  }
+
+  // return function---------------------------
   return (
     <>
       <div className="w-80 h-96 bg-White flex flex-col items-center gap-4 p-4 text-gray">
@@ -15,7 +22,7 @@ function LoginForm() {
 
           <Input type="password" />
         </div>
-        <Button title="ورود" />
+        <Button title="ورود" onClick={submit1} />
         <p className="text-sm">فراموشی رمز عبور</p>
       </div>
     </>
