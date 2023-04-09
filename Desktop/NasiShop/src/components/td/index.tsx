1fc7d5071b3f93f849781fcaddb7af0dc9cf08e8
nastaran-shafie <nastaran.sh33@gmail.com>
2023-04-09 14:43:44 +0430

    fix typescript for td component

--- Desktop/NasiShop/src/components/td/index.tsx
+++ Desktop/NasiShop/src/components/td/index.tsx
@@ -1,6 +1,6 @@
 import { childrenInterface } from "../../types/interface";
 
-function Td({ children }: any) {
+function Td({ children }: childrenInterface) {
   return (
     <td className="p-2 whitespace-nowrap">
       <div className=" text-right text-[10px] md:text-[18px]">{children}</div>
