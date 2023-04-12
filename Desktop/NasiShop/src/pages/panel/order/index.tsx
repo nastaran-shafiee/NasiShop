ade7b89997732f70886711bbe7dad9c631591b05
nastaran-shafie <nastaran.sh33@gmail.com>
2023-04-12 20:56:28 +0430

    feat:add state to order page

--- Desktop/NasiShop/src/pages/panel/order/index.tsx
+++ Desktop/NasiShop/src/pages/panel/order/index.tsx
@@ -2,10 +2,12 @@ import OrderTable from "../../../Layout/oredrTable";
 import { useDispatch, useSelector } from "react-redux";
 import OrderModal from "../../../Layout/orderModal";
 import { FetchSliceData } from "../../../types/interface";
+import { useState } from "react";
 function Orders() {
   const addModal = useSelector(
     (state: FetchSliceData) => state.fetchSlice.orderMode
   );
+
   return (
     <>
       {addModal ? <OrderModal /> : " "}
